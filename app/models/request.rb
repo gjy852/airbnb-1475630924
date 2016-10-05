114f93f661e696f54625fcc0222ddfcecb3964db
Raghu Betina <raghu@firstdraft.com>
2016-10-05 01:33:45 +0000

    Direct association between requests and guest_reviews

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,10 @@
 class Request < ApplicationRecord
   # Direct associations
 
+  has_one    :review_id,
+             :class_name => "GuestReview",
+             :dependent => :destroy
+
   has_one    :listing_availability,
              :class_name => "Reservation",
              :foreign_key => "booked_yes_or_no_id",
