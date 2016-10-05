89b05429a1df016f1e6c8ac628782c5a0130d9e7
Raghu Betina <raghu@firstdraft.com>
2016-10-05 01:33:45 +0000

    Direct association between requests and reservations

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,11 @@
 class Request < ApplicationRecord
   # Direct associations
 
+  has_one    :listing_availability,
+             :class_name => "Reservation",
+             :foreign_key => "booked_yes_or_no_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,9 @@
 class Reservation < ApplicationRecord
   # Direct associations
 
+  belongs_to :booked_yes_or_no,
+             :class_name => "Request"
+
   has_one    :listing_review,
              :foreign_key => "reservations_id",
              :dependent => :destroy
