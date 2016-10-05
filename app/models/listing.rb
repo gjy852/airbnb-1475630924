ccbf4d0e4ab8fd432ee99b5e4cd2294a2e85659d
Raghu Betina <raghu@firstdraft.com>
2016-10-05 01:33:45 +0000

    Direct association between listings and bookmarks

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,9 @@
 class Listing < ApplicationRecord
   # Direct associations
 
+  has_one    :bookmark,
+             :dependent => :destroy
+
   has_many   :listing_availabilities,
              :class_name => "Reservation",
              :dependent => :destroy
