79d42e288073aff3ba6bfe7a9c966ce91ff8808f
Raghu Betina <raghu@firstdraft.com>
2016-10-05 01:35:55 +0000

    Whitelist columns in ListingPhoto for ActiveAdmin

--- app/admin/listing_photo.rb
+++ app/admin/listing_photo.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ListingPhoto do
 
+ permit_params :listing_id, :listing_photos
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
